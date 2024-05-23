283352077ab89709307d66f4bffdb4132eb3df9c
Nicklas Hafiz <hafiznij@hu-berlin.de>
2024-05-23 15:08:46 +0200

    put headers into tabs, fix descriptives bug

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -1,14 +1,14 @@
-file_sources <- list.files(
+dat_names <- list.files(
   path = here::here("data"),
   pattern = "*.csv"
 )
 
-dat_maBerlin <- lapply(here::here("data", file_sources), read.csv)
+dat_maBerlin <- lapply(here::here("data", dat_names), read.csv)
 dat_maBerlin <- lapply(dat_maBerlin, function(x){
   x %>%
     mutate(var_es = se_es^2)
 })
-names(dat_maBerlin) <- file_sources
+names(dat_maBerlin) <- dat_names
 
 dat_maBerlin[["20240115_MABerlin_attention.csv"]][dat_maBerlin[["20240115_MABerlin_attention.csv"]]$Study == "Kuypers & Samyn & Ramaekers (2006)", "Study"] <- "Kuypers, Samyn & Ramaekers (2006)"
 
